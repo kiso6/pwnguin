b6da2eff6d43817841fd44bb07655585a3c3935e
Célian DEBÉTHUNE <67879047+CeliTop@users.noreply.github.com>
2024-02-09 10:28:35 +0100

    [FIX] explookup empty + dependencies

--- modules/explookup.sh
+++ modules/explookup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 
 nmap -oX "detect.xml" -sV $1
-searchsploit --nmap "detect.xml" -j 2> /dev/null | grep -Pzo '{\n\t[\s\S]*?\n}' |  tr '\0' ',' | sed '$ s/.$/]/' > temp.txt && mv temp.txt exploit_list
-# finalize the Parse the JSON
-echo "[" | cat - exploit_list > temp.txt && mv temp.txt exploit_list
-
+searchsploit --nmap "detect.xml" -j 2> /dev/null | grep -Pzo '{\n\t[\s\S]*?\n}' |  tr '\0' ',' > temp.txt && mv temp.txt exploit_list
+# finalize the Parse the JSON, testing if file is empty
+if [ -s "exploit_list" ]; then
+    # file not empty
+    sed '$ s/.$/]/' exploit_list > temp.txt && mv temp.txt exploit_list
+    echo "[" | cat - exploit_list > temp.txt && mv temp.txt exploit_list
+else
+    # file empty
+    echo "[]" > exploit_list
+fi
