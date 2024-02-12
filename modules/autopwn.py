42cfcd75c06936f453ff4f60c40d1ceabfcf2c47
Kilian S <kiliansoual@outlook.fr>
2024-02-12 14:29:02 +0100

    + retriever edb exploits

--- modules/autopwn.py
+++ modules/autopwn.py
@@ -90,16 +90,31 @@ def getEdbExploit(res=[]):
 
     paths = []
     if edbExploits:
+<<<<<<< Updated upstream
         paths = [pwn["Path"] for pwn in edbExploits]
 
     if paths:
         k = 0
+=======
+        for pwn in edbExploits:
+            print(pwn)
+            if not ("(Metasploit)" in pwn["Title"]):
+                paths.append(pwn["Path"])
+
+    pprint.pprint(paths)
+
+    if paths:
+>>>>>>> Stashed changes
         for path in paths:
-            command = "cp " + path + " ./edb/exploit_" + str(k)
+            command = "cp " + path + " ./edb/" + str(path.split("/")[-1])
             print(command)
+<<<<<<< Updated upstream
             subprocess.run(command, shell=True)
             k += 1
     pprint.pprint(paths)
+=======
+            subprocess.run(command,shell=True)
+>>>>>>> Stashed changes
 
 
 def createExploitList(res=[]) -> tuple[list[str], list[str]]:
